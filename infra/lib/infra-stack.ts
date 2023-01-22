4560f15d4d85f30c0fe86d915e6759b915b7d872
billy <aphexlog@gmail.com>
2023-01-22 16:40:59 -0600

    bump cdk-s3-site to v1.0.15

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { HostedSite } from '@elevator-robot/cdk-s3-site';
+// import { HostedSite } from '../../../cdk-s3-site/src/index'
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
