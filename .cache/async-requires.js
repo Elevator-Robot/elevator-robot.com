419ace67f2f261bb47f6bd82da63a06e5b8bd533
billy <aphexlog@gmail.com>
2023-08-05 18:52:45 -0500

    adding amplify.yml

--- .cache/async-requires.js
+++
@@ -1,5 +0,0 @@
-exports.components = {
-  "component---src-pages-404-tsx": () => import("./../../../src/pages/404.tsx" /* webpackChunkName: "component---src-pages-404-tsx" */),
-  "component---src-pages-index-tsx": () => import("./../../../src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */)
-}
-
