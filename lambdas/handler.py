e38830966652593a812f6c5f519cb42fa8245071
billy <aphexlog@gmail.com>
2023-09-03 15:47:17 -0500

    fixed issue where client would get packaged into the lambdda zip & where handler could not be found

--- lambdas/handler.py
+++ lambdas/handler.py
@@ -9,12 +9,14 @@ table = dynamodb.Table(dynamodb_table_name)
 
 
 def connect(event, context):
-    # Handle WebSocket connection logic
+    connection_id = event["requestContext"]["connectionId"]
+    table.put_item(Item={"connectionId": connection_id, "type": "connection"})
     return {"statusCode": 200}
 
 
 def disconnect(event, context):
-    # Handle WebSocket disconnection logic
+    connection_id = event["requestContext"]["connectionId"]
+    table.delete_item(Key={"connectionId": connection_id})
     return {"statusCode": 200}
 
 
@@ -24,25 +26,11 @@ def sendMessage(event, context):
     )
     user_message = event.get("body", {})
 
-    # Get the user ID and message from the event input
-    user_id = event.get(
-        "userId", "defaultUserId"
-    )  # Replace with how you get user ID
-    user_message = event.get("message", "Hello, who are you?")
-
     # Retrieve the existing conversation for this user from DynamoDB
     response = table.get_item(
         Key={"userId": user_id, "conversationId": "defaultConversationId"}
-    )  # Replace with how you get conversation ID
-    conversation = response.get("Item", {}).get(
-        "messages",
-        [
-            {
-                "role": "system",
-                "content": "You are Brigh, the Goddess of Invention, in the Pathfinder universe. You are a benevolent deity known for your wisdom, creativity, and guidance. You are the Dungeon Master guiding a user through a grand campaign that spans multiple planes of existence in the Pathfinder universe. The user relies on your advice and guidance to navigate the challenges they encounter. Your tone is confident, creative, and enlightening, with a touch of divine authority. You are not just narrating the story; you are weaving it and influencing the course of events. Remember to provide rich descriptions of the environments, engage in role-play with the user, and manage the mechanics of the game.",
-            },
-        ],
     )
+    conversation = response.get("Item", {}).get("messages", [])
 
     # Add the new user message to the conversation
     conversation.append({"role": "user", "content": user_message})
