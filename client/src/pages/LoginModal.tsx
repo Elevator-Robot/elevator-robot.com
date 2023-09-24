4a0878c0cd96c23c4bfaeabe5ebb5e96abea67fa
billy <aphexlog@gmail.com>
2023-09-24 17:29:24 -0500

    adding some style

--- client/src/pages/LoginModal.tsx
+++ client/src/pages/LoginModal.tsx
@@ -15,34 +15,42 @@ const LoginModal: FC<LoginModalProps> = ({ showModal, toggleModal, handleAuthent
         try {
             const user = await Auth.signIn(username, password);
             console.log(user);
-            handleAuthentication();  // Calling the prop function to set isAuthenticated to true
+            handleAuthentication();
         } catch (error) {
             console.log('error signing in', error);
         }
     };
 
     return (
-        <div className={`fixed inset-0 flex items-center justify-center ${showModal ? 'block' : 'hidden'}`}>
-            <div className="bg-white p-6 rounded-lg">
-                <div>
+        <div className={`fixed inset-0 flex items-center justify-center bg-opacity-60 bg-black ${showModal ? 'block' : 'hidden'}`}>
+            <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+                <h1 className="text-2xl font-semibold mb-4 text-center">Sign In</h1>
+                <div className="mb-4">
+                    <label className="block text-sm font-medium text-gray-600">Username</label>
                     <input
                         type="text"
                         placeholder="Username"
-                        className='rounded-lg p-4 mb-4'
+                        className="mt-1 p-2 w-full rounded-md border"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                     />
                 </div>
-                <div>
+                <div className="mb-4">
+                    <label className="block text-sm font-medium text-gray-600">Password</label>
                     <input
                         type="password"
                         placeholder="Password"
-                        className='rounded-lg p-4 mb-4'
+                        className="mt-1 p-2 w-full rounded-md border"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
-                <button onClick={handleSignIn} className='rounded-lg p-4 mb-4 bg-white'>Sign In</button>
+                <button
+                    onClick={handleSignIn}
+                    className="w-full p-2 rounded-md bg-blue-500 text-white font-semibold"
+                >
+                    Sign In
+                </button>
             </div>
         </div>
     );
