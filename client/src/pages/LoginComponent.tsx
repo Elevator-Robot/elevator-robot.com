12f4ceb81b8c5e6ff0443b975a0faa154b8d91ba
billy <aphexlog@gmail.com>
2023-09-24 10:37:17 -0500

    username now replaces login button post authentication

--- client/src/pages/LoginComponent.tsx
+++ client/src/pages/LoginComponent.tsx
@@ -1,54 +1,56 @@
 import React, { useState } from 'react';
 import { Auth } from 'aws-amplify';
-import amplifyConfig from '../../amplify-config';
 
-Auth.configure(amplifyConfig);
+interface Props {
+	onAuthChange: (isAuthenticated: boolean, username: string | null) => void;
+}
 
-const LoginComponent: React.FC = () => {
+const LoginComponent: React.FC<Props> = ({ onAuthChange }) => {
 	const [username, setUsername] = useState<string>('');
 	const [password, setPassword] = useState<string>('');
-	const [user, setUser] = useState<any>(null);
 
 	const handleSignIn = async () => {
     try {
+      // Assuming Auth.signIn is available via a parent component or context
 		const userData = await Auth.signIn(username, password);
-		setUser(userData);
+		onAuthChange(true, userData.username);
 		console.log('Successfully signed in:', userData);
     } catch (error) {
-    console.error('Error signing in:', error);
-    }};
+		console.error('Error signing in:', error);
+    }
+};
 
 	const handleSignOut = async () => {
-    try {
+		try {
+		// Assuming Auth.signOut is available via a parent component or context
 		await Auth.signOut();
-		setUser(null);
+		onAuthChange(false, null);
 		console.log('Successfully signed out');
-    } catch (error) {
-    console.error('Error signing out:', error);
-    }};
+		} catch (error) {
+		console.error('Error signing out:', error);
+		}
+	};
 
 	return (
-    <div>
-    {user ? (
-        <button onClick={handleSignOut}>Sign Out</button>
-    ) : (
-        <div>
-        <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleSignIn}>Sign In</button>
-        </div>
-    )}
-    </div>
+		<div>
+			<div>
+				<input
+					type="text"
+					placeholder="Username"
+					className='rounded-lg p-4 mb-4'
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					className='rounded-lg p-4 mb-4'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button onClick={handleSignIn} className='rounded-lg p-4 mb-4 bg-white'>Sign In</button>
+			</div>
+		</div>
 	);
 };
 
--- client/src/pages/ToolBarComponent.tsx
+++ client/src/pages/ToolBarComponent.tsx
@@ -1,20 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoginComponent from './LoginComponent';
+import { Auth } from 'aws-amplify';
 
 const ToolBarComponent: React.FC = () => {
-    const [showLogin, setShowLogin] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [username, setUsername] = useState<string | null>(null);
 
-    const toggleLogin = () => {
-        setShowLogin(!showLogin);
+    const handleAuthChange = (isAuth: boolean, uname: string | null) => {
+        setIsAuthenticated(isAuth);
+        setUsername(uname);
+    };
+
+    useEffect(() => {
+        checkUserAuthentication();
+    }, []);
+
+    const checkUserAuthentication = async () => {
+        try {
+            const user = await Auth.currentAuthenticatedUser();
+            setIsAuthenticated(true);
+            setUsername(user.username);
+        } catch (error) {
+            setIsAuthenticated(false);
+        }
     };
 
     return (
         <>
-        <button 
-            data-drawer-target="default-sidebar" 
-            data-drawer-toggle="default-sidebar" 
-            aria-controls="default-sidebar" 
-            type="button" 
+        <button
+            data-drawer-target="default-sidebar"
+            data-drawer-toggle="default-sidebar"
+            aria-controls="default-sidebar"
+            type="button"
             className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
             <span className="sr-only">Open sidebar</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -22,19 +39,19 @@ const ToolBarComponent: React.FC = () => {
             </svg>
         </button>
 
-            <aside id="default-sidebar" className="fixed top-0 right-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                    <ul className="space-y-2 font-medium">
-                        <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group" onClick={toggleLogin}>
-                                <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
-                                </svg>
-                            <span className="flex-1 ml-3 whitespace-nowrap">Login</span>
-                            </a>
-                        </li>
-                    </ul>
-                {showLogin && <LoginComponent />}
+        <aside id="default-sidebar" className="fixed top-0 right-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0">
+            <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                <ul className="space-y-2 font-medium">
+                    <li>
+                        <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group">
+                            <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
+                            </svg>
+                            <span className="flex-1 ml-3 whitespace-nowrap">{isAuthenticated ? username : "Login"}</span>
+                        </a>
+                    </li>
+                </ul>
+                {!isAuthenticated && <LoginComponent onAuthChange={handleAuthChange} />}
             </div>
         </aside>
         </>
