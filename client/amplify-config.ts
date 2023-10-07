b550b0e7ae19cbdd1bd1a6516646cd6663f1d6a5
billy <aphexlog@gmail.com>
2023-10-07 13:18:08 -0500

    adding field for email verification code

--- client/amplify-config.ts
+++ client/amplify-config.ts
@@ -2,9 +2,9 @@ const amplifyConfig = {
     Auth: {
         region: 'us-east-1',
         // userPoolId: 'us-east-1_Up8p1y5hH',
-        userPoolId: 'us-east-1_LCKx0Jf0y',
+        userPoolId: 'us-east-1_csFdMVNv5',
         // userPoolWebClientId: '1d4im45bcah4jqhkumj86rnits',
-        userPoolWebClientId: '2fujcn9j85hnrvqoiv5qkk0oi4'
+        userPoolWebClientId: '13r7h6j2qr7hbgau5mj1ftrpu7'
     },
 };
 
--- client/src/pages/LoginModal.tsx
+++ client/src/pages/LoginModal.tsx
@@ -10,44 +10,61 @@ interface LoginModalProps {
 const LoginModal: FC<LoginModalProps> = ({ showModal, toggleModal, handleAuthentication }) => {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
     const [isSignUp, setIsSignUp] = useState<boolean>(false);
     const [verifyPassword, setVerifyPassword] = useState<string>('');
+    const [verificationCode, setVerificationCode] = useState<string>('');
+    const [showVerificationInput, setShowVerificationInput] = useState<boolean>(false);
 
+
+    const confirmSignUp = async (email: string, code: string) => {
+        try {
+            await Auth.confirmSignUp(email, code);
+            setShowVerificationInput(false); // Hide the verification input once completed
+            console.log('Code accepted.');
+        } catch (error) {
+            console.error('Error confirming sign up', error);
+        }
+    }
+    
 
     const keyPressHandler = async (e: React.KeyboardEvent) => {
         if (e.key === 'Enter') {
-            await handleSignIn(username, password);
+            await handleSignIn(email, password);
         }
     };
 
-    const handleSignIn = async (username: string, password: string) => {
+    const handleSignIn = async (email: string, password: string) => {
         try {
-            await Auth.signIn(username, password);
+            await Auth.signIn({
+                username: email, // Use email for signing in.
+                password
+            });
+            
             handleAuthentication(); // This should update isAuthenticated to true
         } catch (error) {
             console.error("Authentication error:", error);
         }
     };
 
-    const handleSignUp = async (username: string, password: string) => {
+    const handleSignUp = async (email: string, password: string) => {
         if(password !== verifyPassword) {
             console.error("Passwords do not match!");
             return;
         }
-
+    
         try {
             await Auth.signUp({
-                username,
+                username: email,
                 password,
-                attributes: {
-                    email: username,
-                },
+                attributes: { email },
             });
-            await handleSignIn(username, password);
+            setShowVerificationInput(true);
         } catch (error) {
             console.error("Authentication error:", error);
         }
     }
+    
 
     return (
         <div className={`bg-blue-200 fixed inset-0 flex items-center justify-center ${showModal ? 'block' : 'hidden'}`}>
@@ -68,13 +85,13 @@ const LoginModal: FC<LoginModalProps> = ({ showModal, toggleModal, handleAuthent
                 </div>
 
                 <div className="mb-4">
-                    <label className="block text-sm font-medium text-gray-600">Username</label>
+                    <label className="block text-sm font-medium text-gray-600">Email</label>
                     <input
                         type="text"
-                        placeholder="Username"
+                        placeholder="Email"
                         className="mt-1 p-2 w-full rounded-md border"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         onKeyDown={keyPressHandler} // respond to Enter key
                     />
                 </div>
@@ -104,15 +121,40 @@ const LoginModal: FC<LoginModalProps> = ({ showModal, toggleModal, handleAuthent
                     </>
                 )}
 
+                {showVerificationInput && (
+                    <>
+                        <div className="mb-4">
+                            <label className="block text-sm font-medium text-gray-600">Verification Code</label>
+                            <input
+                                type="text"
+                                placeholder="Enter Verification Code"
+                                className="mt-1 p-2 w-full rounded-md border"
+                                value={verificationCode}
+                                onChange={(e) => setVerificationCode(e.target.value)}
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <button
+                                onClick={() => confirmSignUp(email, verificationCode)}
+                                className="w-full p-2 rounded-md bg-blue-500 text-white font-semibold"
+                                disabled={verificationCode.length === 0}
+                            >
+                                Confirm Code
+                            </button>
+                        </div>
+                    </>
+                )}
+
         <div className="flex justify-between items-center mb-4">
             <button
-                onClick={isSignUp ? () => handleSignUp(username, password) : () => handleSignIn(username, password)}
+                onClick={isSignUp ? () => handleSignUp(email, password) : () => handleSignIn(email, password)}
                 className="w-full p-2 rounded-md bg-blue-500 text-white font-semibold"
-                disabled={username.length === 0 || password.length === 0}
+                disabled={email.length === 0 || password.length === 0}
             >
             {isSignUp ? 'Sign Up' : 'Sign In'}
             </button>
         </div>
+
 
         </div>
     </div>
