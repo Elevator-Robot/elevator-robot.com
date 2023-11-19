3564f4adad9602edea01fb011b4bc78dfa126547
billy <aphexlog@gmail.com>
2023-11-19 17:10:15 -0600

    updating client wss address

--- client/amplify-config.ts
+++ client/amplify-config.ts
@@ -1,10 +1,8 @@
 const amplifyConfig = {
     Auth: {
         region: 'us-east-1',
-        // userPoolId: 'us-east-1_Up8p1y5hH',
-        userPoolId: 'us-east-1_csFdMVNv5',
-        // userPoolWebClientId: '1d4im45bcah4jqhkumj86rnits',
-        userPoolWebClientId: '13r7h6j2qr7hbgau5mj1ftrpu7'
+        userPoolId: 'us-east-1_51h57khs7',
+        userPoolWebClientId: '1jnvr69msa4832ggv94li9riee'
     },
 };
 
