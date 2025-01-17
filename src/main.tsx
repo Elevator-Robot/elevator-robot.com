5adc9b29f687ac930a129bd36c7a8fd83d63947b
Aaron West <aphexlog@gmail.com>
2025-01-16 18:12:10 -0600

    fully removed BE

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
 import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
