9d39b2b54c211413b6f49bea7b08e80c56c220b4
Aaron West <aphexlog@gmail.com>
2025-01-22 14:49:29 -0600

    adding sms be service

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,3 +1,5 @@
+import * as ses from 'aws-cdk-lib/aws-ses';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
@@ -5,7 +7,15 @@ import { data } from './data/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
-  auth,
-  data,
+const backend = defineBackend({ auth, data });
+const customResourceStack = backend.createStack('CustomEmailResources');
+
+const emailIdentity = new ses.EmailIdentity(customResourceStack, 'EmailIdentity', {
+  identity: ses.Identity.email('aaron@elevator-robot.com'),
+});
+
+// Create a policy statement
+new iam.PolicyStatement({
+  actions: ['ses:SendEmail', 'ses:SendRawEmail'],
+  resources: ['*'], // Adjust the resource ARN as needed
 });
