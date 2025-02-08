1f3627a62dbf18461b4a630dce9f420204fd27dd
Aaron West <aphexlog@gmail.com>
2025-02-08 17:47:59 -0600

    feat: Add contact form schema with email sending trigger

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,62 +1,24 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 import { sendEmail } from '../functions/send-email/resource'
 
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
-and "delete" any "Todo" records.
-=========================================================================*/
-const schema = a.schema({
-  sendMessage: a
-    .query()
-    .arguments({
-      name: a.string(),
-      email: a.string(),
-      message: a.string(),
-    })
-    .returns(a.string())
-    .authorization(allow => [
-      allow.guest()
-    ])
-    .handler(a.handler.function(sendEmail))
-});
+// create a schema for sneding an ses email from the contact form (name, email, message)
 
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+export const contactSchema = defineData<ClientSchema>({
+  name: 'contact',
+  fields: {
+    name: a.string(),
+    email: a.string(),
+    message: a.string(),
+  },
+  syncConfig: {
+    conflictDetection: 'VERSION',
+    conflictHandler: 'AUTOMERGE',
+  },
+  timestamps: true,
+  functionTriggers: {
+    onCreate: [sendEmail],
   },
 });
 
+export type Schema = ClientSchema<typeof contactSchema>;
 
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
