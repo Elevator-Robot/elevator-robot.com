7b30aeb821eaca05339bbbb6dce683ef211ab01b
Aaron West <aphexlog@gmail.com>
2025-02-16 13:15:25 -0600

    adding dev console logging

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Terminal from 'vite-plugin-terminal';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['react-devtools-core']
-  }
-})
+  plugins: [
+    react(),
+    Terminal({
+      console: 'terminal', // Redirect console logs to the terminal
+      output: ['terminal', 'console'], // Optionally log to both terminal and browser console
+    }),
+  ],
+});
