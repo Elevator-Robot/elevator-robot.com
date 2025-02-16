1f38e6f4ec281caaaa8904ae9fbd64e52fd28bee
Aaron West <aphexlog@gmail.com>
2025-02-16 13:39:16 -0600

    fix: Update email addresses in send-email Lambda handler

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,18 +3,31 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { sendEmail } from './functions/send-email/resource';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 const backend = defineBackend({ auth, data, sendEmail });
 
+const sendemailFunction = backend.sendEmail.resources.lambda
+
 const customResourceStack = backend.createStack('CustomEmailResources');
 
 const environment = process.env.ENV || 'dev';
 
-new ses.EmailIdentity(customResourceStack, `EmailIdentity-${environment}`, {
+const emailIdentity = new ses.EmailIdentity(customResourceStack, `EmailIdentity-${environment}`, {
   identity: ses.Identity.email('aphexlog@gmail.com'),
 });
+
+const statement = new PolicyStatement({
+  sid: 'AllowSendEmail',
+  actions: ['SES:SendEmail'],
+  resources: [emailIdentity.emailIdentityArn],
+});
+
+sendemailFunction.addToRolePolicy(statement);
+
 
 export default backend;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import Terminal from 'vite-plugin-terminal';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    Terminal({
-      console: 'terminal', // Redirect console logs to the terminal
-      output: ['terminal', 'console'], // Optionally log to both terminal and browser console
-    }),
-  ],
-});
+  plugins: [react()],
+  // optimizeDeps: {
+  //   exclude: ['react-devtools-core']
+  // }
+})
