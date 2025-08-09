f928844ff6ae56e27dee5b746abbb8a723eacc88
Aaron West <aphexlog@github.com>
2025-08-09 13:37:04 -0500

    adding dark/light mode (dark default)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,css}",
