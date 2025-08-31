bc08b2531f431ac3d2d52736dce92804025c0784
Aaron West <aphexlog@github.com>
2025-08-31 17:47:16 -0500

    detaching ses from stack

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -5,19 +5,16 @@ import { data } from './data/resource';
 import { sendEmail } from './functions/send-email/resource';
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
-
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
 const backend = defineBackend({ auth, data, sendEmail });
 
-const customResourceStack = backend.createStack('CustomEmailResources');
-
-const environment = process.env.ENV || 'dev';
-
-const emailIdentity = new ses.EmailIdentity(customResourceStack, `EmailIdentity-${environment}`, {
-  identity: ses.Identity.email('aphexlog@gmail.com'),
-});
+// Only create SES resources on main branch
+if (process.env.AWS_BRANCH === 'main') {
+  const customResourceStack = backend.createStack('SharedEmailResources');
+  
+  const emailIdentity = new ses.EmailIdentity(customResourceStack, 'SharedEmailIdentity', {
+    identity: ses.Identity.email('aphexlog@gmail.com'),
+  });
+}
 
 const sendEmailFunction = backend.sendEmail.resources.lambda;
 
